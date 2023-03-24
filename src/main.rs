805407f1c22ce0f7bd7beba46a39138fe950004d
Bailig <barrybailig@hotmail.com>
2023-03-24 22:52:21 +0300

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 mod algorithms;
 
-use algorithms::array;
-use algorithms::linked_list;
-
 fn main() {
-    array::run();
-    linked_list::run();
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    if args.len() != 1 {
+        println!("Missing argument: algorithms or exercises");
+        std::process::exit(1);
+    }
+    let arg = &args[0];
+    if arg == "algorithms" {
+        algorithms::array::run();
+        algorithms::linked_list::run();
+    } else if arg == "exercises" {
+    } else {
+        println!("Unknown argument: {}", arg);
+        std::process::exit(1);
+    }
 }
